91b5c184fee182ad0ff97173caad68f4126a07d0
Priscila Oliveira Moraes <101437854+PriscilaOM@users.noreply.github.com>
2023-07-16 20:58:50 -0300

    Update 00_declarando_dicionarios.py

--- 01 - Estrutura de dados/04 - Dicionários/00_declarando_dicionarios.py
+++ 01 - Estrutura de dados/04 - Dicionários/00_declarando_dicionarios.py
@@ -1,8 +1,11 @@
-pessoa = {"nome": "Guilherme", "idade": 28}
+pessoa = {"nome": "Priscila", "idade": 32}
 print(pessoa)
 
-pessoa = dict(nome="Guilherme", idade=28)
+pessoa = dict(nome="Priscila", idade=32)
 print(pessoa)
 
 pessoa["telefone"] = "3333-1234"  # {"nome": "Guilherme", "idade": 28, "telefone": "3333-1234"}
+print(pessoa)
+
+pessoa["nome"]="Benjamin"
 print(pessoa)
