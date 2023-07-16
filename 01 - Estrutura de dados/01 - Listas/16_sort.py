ac5b82f95dccf60591a0f057256a0a78800e2862
Priscila Oliveira Moraes <101437854+PriscilaOM@users.noreply.github.com>
2023-07-16 20:51:42 -0300

    Update 16_sort.py

--- 01 - Estrutura de dados/01 - Listas/16_sort.py
+++ 01 - Estrutura de dados/01 - Listas/16_sort.py
@@ -1,15 +1,15 @@
-linguagens = ["python", "js", "c", "java", "csharp"]
+linguagens = ["python", "js", "c", "java", "csharp"] 
 linguagens.sort()  # ["c", "csharp", "java", "js", "python"]
-print(linguagens)
+print(linguagens)#ordena alfabeticamente
 
 linguagens = ["python", "js", "c", "java", "csharp"]
 linguagens.sort(reverse=True)  # ["python", "js", "java", "csharp", "c"]
-print(linguagens)
+print(linguagens)#ordena alfabeticamente ao contrario
 
 linguagens = ["python", "js", "c", "java", "csharp"]
 linguagens.sort(key=lambda x: len(x))  # ["c", "js", "java", "python", "csharp"]
-print(linguagens)
-
+print(linguagens) #ordena por tamanho da palavra função anônima que não tem nome
+#len tira o tamanho de uma string
 linguagens = ["python", "js", "c", "java", "csharp"]
 linguagens.sort(key=lambda x: len(x), reverse=True)  # ["python", "csharp", "java", "js", "c"]
 print(linguagens)
