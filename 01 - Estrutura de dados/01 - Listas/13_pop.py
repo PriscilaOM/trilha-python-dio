1820cac8486a994a20c3735dc2eca3bc320d2f30
Priscila Oliveira Moraes <101437854+PriscilaOM@users.noreply.github.com>
2023-07-16 20:50:38 -0300

    Update 13_pop.py

--- 01 - Estrutura de dados/01 - Listas/13_pop.py
+++ 01 - Estrutura de dados/01 - Listas/13_pop.py
@@ -4,3 +4,6 @@ print(linguagens.pop())  # csharp
 print(linguagens.pop())  # java
 print(linguagens.pop())  # c
 print(linguagens.pop(0))  # python
+print(linguagens.pop())
+#tira o último elemento da lista
+print(linguagens)
