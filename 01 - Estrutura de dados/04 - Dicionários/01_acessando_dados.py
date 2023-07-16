ba9428f1a0cae4a7204942db82696ea2026cd0c9
Priscila Oliveira Moraes <101437854+PriscilaOM@users.noreply.github.com>
2023-07-16 20:59:11 -0300

    Update 01_acessando_dados.py

--- 01 - Estrutura de dados/04 - Dicionários/01_acessando_dados.py
+++ 01 - Estrutura de dados/04 - Dicionários/01_acessando_dados.py
@@ -1,11 +1,12 @@
-dados = {"nome": "Guilherme", "idade": 28, "telefone": "3333-1234"}
+dados = {"nome": "Pri", "idade": 32, "telefone": "3333-1234"}
 
-print(dados["nome"])  # "Guilherme"
-print(dados["idade"])  # 28
+print(dados["nome"])  # "Pri"
+print(dados["idade"])  # 32
 print(dados["telefone"])  # "3333-1234"
 
-dados["nome"] = "Maria"
-dados["idade"] = 18
+dados["nome"] = "Benjamin"
+dados["idade"] = 5
 dados["telefone"] = "9988-1781"
 
 print(dados)  # {"nome": "Maria", "idade": 18, "telefone": "9988-1781"}
+
