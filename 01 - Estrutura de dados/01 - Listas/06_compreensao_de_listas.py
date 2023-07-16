0ac0f6a85f57ffc544aaf13079ae7882a9c8d256
Priscila Oliveira Moraes <101437854+PriscilaOM@users.noreply.github.com>
2023-07-16 20:48:32 -0300

    Update 06_compreensao_de_listas.py

--- 01 - Estrutura de dados/01 - Listas/06_compreensao_de_listas.py
+++ 01 - Estrutura de dados/01 - Listas/06_compreensao_de_listas.py
@@ -1,9 +1,22 @@
 # Filtrar lista
 numeros = [1, 30, 21, 2, 9, 65, 34]
 pares = [numero for numero in numeros if numero % 2 == 0]
+#1°parte retorno 2°parte e a iteração 3° parte if
+
 print(pares)
 
+for numero in numeros:
+    if numero % 2 == 0:
+        pares.append(numero)
+
+print(pares)    
+    
 # Modificar valores
 numeros = [1, 30, 21, 2, 9, 65, 34]
 quadrado = [numero**2 for numero in numeros]
 print(quadrado)
+
+
+for numero in numeros:
+    quadrado.append(numero ** 2)
+    print(quadrado)
