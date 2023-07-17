3ceeff81b6cfd681b97786cb565e7ff2387355fd
Priscila Oliveira Moraes <101437854+PriscilaOM@users.noreply.github.com>
2023-07-16 21:04:35 -0300

    Update 06_objetos_de_primeira_classe.py

--- 01 - Estrutura de dados/05 - Funções/06_objetos_de_primeira_classe.py
+++ 01 - Estrutura de dados/05 - Funções/06_objetos_de_primeira_classe.py
@@ -1,10 +1,26 @@
 def somar(a, b):
     return a + b
 
+def multiplicar(a,b):
+    return a * b
+
+def subtrair(a,b):
+    return a - b
+
+def dividir(a,b):
+    return a / b
+
 
 def exibir_resultado(a, b, funcao):
     resultado = funcao(a, b)
-    print(f"O resultado da operação {a} + {b} = {resultado}")
+    print(f"O resultado da operação = {resultado}")
 
 
 exibir_resultado(10, 10, somar)  # O resultado da operação 10 + 10 = 20
+exibir_resultado(10,2,multiplicar)
+exibir_resultado(5,2,subtrair)
+#função chamando função
+
+op = somar
+
+print(op(1,23))
