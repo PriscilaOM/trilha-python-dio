59ba395e7fb8bcb67c3fafba74ae70ed460ac45e
Priscila Oliveira Moraes <101437854+PriscilaOM@users.noreply.github.com>
2023-07-16 21:04:00 -0300

    Update 05_parametros_somente_por_nome.py

--- 01 - Estrutura de dados/05 - Funções/05_parametros_somente_por_nome.py
+++ 01 - Estrutura de dados/05 - Funções/05_parametros_somente_por_nome.py
@@ -1,6 +1,12 @@
 def criar_carro(modelo, ano, placa, /, *, marca, motor, combustivel):
     print(modelo, ano, placa, marca, motor, combustivel)
+#hibrido
 
+criar_carro("Palio", 1999, "ABC-1234", marca="Fiat", motor="1.0", combustivel="Gasolina")
+#criar_carro(modelo="Palio", ano=1999, placa="ABC-1234", marca="Fiat", motor="1.0", combustivel="Gasolina")  # inválido
 
-# criar_carro("Palio", 1999, "ABC-1234", marca="Fiat", motor="1.0", combustivel="Gasolina")
-criar_carro(modelo="Palio", ano=1999, placa="ABC-1234", marca="Fiat", motor="1.0", combustivel="Gasolina")  # inválido
+def criar_carro2(*,modelo, ano, placa, marca, motor, combustivel):
+    print(modelo, ano, placa, marca, motor, combustivel)
+    
+#somente com keyword nomes e posições
+criar_carro2(modelo="Prisma", ano=2023, placa="POL-9E08", marca="Chevrolet", motor="1.4", combustivel="Etanol")
